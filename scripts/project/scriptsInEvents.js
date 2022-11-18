ae9ca79e5389919732037f20bdc271070e8040c0
mjm <mjm121273@gmail.com>
2022-11-18 22:35:17 +0330

    fire mode added

--- scripts/project/scriptsInEvents.js
+++ scripts/project/scriptsInEvents.js
@@ -35,9 +35,54 @@ function numentofa(n) {
 
 const scriptsInEvents = {
 
-	async EventSheet1_Event32(runtime, localVars)
+	async EventSheet1_Event39(runtime, localVars)
 	{
 		runtime.globalVars.temp=toFarsiNumber(localVars.num);
+	},
+
+	async Gboks_Event3_Act1(runtime, localVars)
+	{
+		
+		
+		//runtime.globalVars.TemporaryCoins = window.parent.userScore;
+		//if(window.parent.userScore==null)
+		//{
+		//runtime.globalVars.TemporaryCoins=0;
+		//}
+		
+		
+		//runtime.globalVars.initScore = window.parent.userScore;
+		
+		gboks.gameInit(function (e) {
+		   e || gboks.gameReady();
+		});
+		
+		//gboks.emitter.addListener('start', (e) => {	
+			
+		//	runtime.globalVars.start = true;
+			
+			//runtime.globalVars.ajaxHeader=window.parent.csrfToken;
+		//});
+		
+	},
+
+	async Gboks_Event6_Act1(runtime, localVars)
+	{
+		// to do send score
+		var tmp=runtime.globalVars.score;
+		window.parent.gboks.emit('gameOver', tmp);
+		
+		
+	},
+
+	async Gboks_Event6_Act3(runtime, localVars)
+	{
+		// to do send score
+		
+		window.parent.gboks.emit('gameEnd');
+		
+		
+		
 	}
 
 };
